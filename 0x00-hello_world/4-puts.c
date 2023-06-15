a20bca54581665b0c3b87afaca2f18cc53c92f9f
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-15 09:33:21 -0700

    meow

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main -  is the entry point
+ * main - is the entry point
  * return: 0 if succeeded
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
