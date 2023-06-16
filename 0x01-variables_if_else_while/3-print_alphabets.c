0499ccb982753713418dd8b7fd589eb577c1238b
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-16 10:01:01 -0700

    meow

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return: returns 0 if succeeded
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char letter;
+	char meow;
 
 	for (meow = 'a'; meow <= 'z'; meow++)
 		putchar(meow);
