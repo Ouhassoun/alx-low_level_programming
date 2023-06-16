4964e2071e7f878340b65a769dde328b9f221788
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-16 09:45:55 -0700

    meow

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: returns 0 if succeeded
  */
+
 int main(void)
 {
-        int nb1, nb2, nb3;
+	int nb1, nb2, nb3;
         for (nb1 = 0; nb1 < 8; nb1++)
         {
                 for (nb2 = nb1 + 1; nb2 < 9; nb2++)
@@ -16,13 +18,17 @@ int main(void)
                                 putchar((nb1 % 10) + '0');
                                 putchar((nb2 % 10) + '0');
                                 putchar((nb3 % 10) + '0');
+
                                 if (nb1 == 7 && nb2 == 8 && nb3 == 9)
                                         continue;
+
                                 putchar(',');
                                 putchar(' ');
                         }
                 }
         }
+
         putchar('\n');
+
         return (0);
 }
