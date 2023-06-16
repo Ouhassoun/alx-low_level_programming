e16e4915183444664bda56dc9af0335f79ee0c7a
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-16 09:40:26 -0700

    meow

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int nb1, nb2, nb3;
-
 	for (nb1 = 0; nb1 < 8; nb1++)
 	{
 		for (nb2 = nb1 + 1; nb2 < 9; nb2++)
@@ -19,17 +18,13 @@ int main(void)
 				putchar((nb1 % 10) + '0');
 				putchar((nb2 % 10) + '0');
 				putchar((nb3 % 10) + '0');
-
 				if (nb1 == 7 && nb2 == 8 && nb3 == 9)
 					continue;
-				
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
