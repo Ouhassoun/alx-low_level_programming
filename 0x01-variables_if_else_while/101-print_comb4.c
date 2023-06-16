ea262efebde1d5c9e4ff995c517436ba6b32d232
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-16 09:43:54 -0700

    meow

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,4 @@
-
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,23 +6,23 @@
  */
 int main(void)
 {
-	int nb1, nb2, nb3;
-	for (nb1 = 0; nb1 < 8; nb1++)
-	{
-		for (nb2 = nb1 + 1; nb2 < 9; nb2++)
-		{
-			for (nb3 = nb2 + 1; nb3 < 10; nb3++)
-			{
-				putchar((nb1 % 10) + '0');
-				putchar((nb2 % 10) + '0');
-				putchar((nb3 % 10) + '0');
-				if (nb1 == 7 && nb2 == 8 && nb3 == 9)
-					continue;
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        int nb1, nb2, nb3;
+        for (nb1 = 0; nb1 < 8; nb1++)
+        {
+                for (nb2 = nb1 + 1; nb2 < 9; nb2++)
+                {
+                        for (nb3 = nb2 + 1; nb3 < 10; nb3++)
+                        {
+                                putchar((nb1 % 10) + '0');
+                                putchar((nb2 % 10) + '0');
+                                putchar((nb3 % 10) + '0');
+                                if (nb1 == 7 && nb2 == 8 && nb3 == 9)
+                                        continue;
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
