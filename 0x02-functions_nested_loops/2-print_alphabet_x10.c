d6d95c9d3ecd650c4476642d4581a20a218441b5
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 13:12:03 -0700

    meow

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * main: entry point
+ * print_alphabet_x10:function that prints 10 times the alphabet, in lowercase, followed by a new line
  * Return: void
  */
 
@@ -9,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
