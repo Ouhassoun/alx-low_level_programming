3332f4cc77ff38caffffa254fe637f1785196cac
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 13:24:21 -0700

    meow

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,12 @@
 
 /**
  * _abs - computes the absolute value of an integer
- *
+ * @nb: fonction's parameter
  * Return: integer
  */
 
-int _abs(int nb) {
+int _abs(int nb) 
+{
 
 	if (nb < 0)
 		return -nb;
