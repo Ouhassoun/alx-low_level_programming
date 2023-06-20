c84cf0a876a416e8a47b34003b69964bb6472045
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 13:15:55 -0700

    meow

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10:function that prints 10 times the alphabet
+ * print_alphabet_x10 - function that prints 10 times the alphabet
  * Return: void
  */
 
