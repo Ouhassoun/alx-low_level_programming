f5e2fb11bd15a7953be50a4d5002e3416a9891fa
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 14:57:33 -0700

    meow

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned int nb1 = 1;
+	unsigned int nb2 = 2;
+
+	printf("%u, %u", nb1, nb2);
+	int i;
+
+	for (i = 3; i <= 98; ++i)
+	{
+		unsigned int nb = nb1 + nb2;
+
+		printf(", %u", nb);
+
+		nb1 = nb2;
+		nb2 = nb;
+	}
+
+	printf("\n");
+
+	return (0);
+}
