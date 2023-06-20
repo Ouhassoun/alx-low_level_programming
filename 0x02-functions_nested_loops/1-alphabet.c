05397a5cfcd3f2bbc2fa8ee9f1d099bbc5dc3011
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 13:03:22 -0700

    meow

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char meow = 'a';
+	
+	while (meow <= 'z')
+	{
+		_putchar(meow);
+		meow++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: integer
+ */
+
+int _abs(int nb) {
+
+	if (nb < 0)
+		return -nb;
+	else
+		return nb;
+}
