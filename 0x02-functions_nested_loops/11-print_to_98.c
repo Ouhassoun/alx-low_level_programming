38c609bcab5d79f7f631a98d64ec4c1c77924477
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 13:55:10 -0700

    meow

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
     	if (n <= 98)
 	{
-		for (int i = n; i <= 98; ++i)
+		for (int i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (int i = n; i >= 98; --i)
+		for (int i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
