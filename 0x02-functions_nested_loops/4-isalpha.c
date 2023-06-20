347c5e5391cb8b96d775ee521acd082d7ad0eb48
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-20 13:19:47 -0700

    meow

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,11 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: c is an ascii character
  * Return: 1 if c is a letter lowercase or uppercase, 0 othewise
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
