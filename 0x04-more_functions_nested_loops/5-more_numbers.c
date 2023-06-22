c8931faf1477402990e01c011d37ca70426c2efe
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-22 09:05:14 -0700

    meow

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,21 +2,27 @@
 
 /**
  * more_numbers - entry point
+ *
  * Description: Prints the numbers with _putchar
+ *
  * Return: void
  */
 
 void more_numbers(void)
 {
-	int nb, row;
+	int number, tens, units, row;
 
-	for(row=0; row<10; row++)
+	for (row = 1; row <= 10; row++)
 	{
-		for(nb=0; nb<=14; nb++)
+		for (number = 0; number <= 14; number++)
 		{
-			_putchar(nb);
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
