c8fe3c99df554e3cbae201d2322b935d9540df15
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-25 21:01:58 -0700

    meow

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * reset_to_98 - that takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - that takes a pointer to an int as parameter
  * @n: input pointer
  * Return : void
  */
