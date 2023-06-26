291265df1fa005d5e6e5565301457d357ebea81f
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-25 21:11:11 -0700

    meow

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* swap_int - swaps the values of two intergers
+* @a: int variable
+* @b: int variable
+* @temp: temporary
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
