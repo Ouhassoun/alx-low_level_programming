2d52ef393cadacfbd288b051fd4a8fe24f0ee8ff
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-25 23:13:24 -0700

    meow

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,27 +9,38 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
+	int numDigits = 0;
+	int numSigns = 0;
 	int result = 0;
+	int hasNumber = 0;
+	int isNegative = 0;
+	int i = 0;
 
-	while (*s == ' ')
+	while (s[numDigits] != '\0')
 	{
-		s++;
+		if (s[numDigits] >= '0' && s[numDigits] <= '9')
+			hasNumber = 1;
+		else if (s[numDigits] == '-')
+		{
+			numSigns++;
+			isNegative = 1;
+		}
+		else if (s[numDigits] != '+')
+			break;
+		numDigits++;
+	}
+		if (!hasNumber || numSigns > 1)
+			return (0);
+	while (i < numDigits)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = (result * 10) + (s[i] - '0');
+			i++;
+		}
+		else
+			break;
 	}
 
-	if (*s == '+' || *s == '-')
-	{
-		sign = (*s == '-') ? -1 : 1;
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-		int digit = *s - '0';
-
-		result = ((result * 10) + digit);
-		s++;
-	}
-
-	return (result * sign);
+	return (isNegative ? -result : result);
 }
