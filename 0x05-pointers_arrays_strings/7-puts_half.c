e1b1f7f61b3184cd85d476b07b533ee5bf02557c
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-25 21:58:53 -0700

    meow

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * puts_half - prints half of a string, followed by a new line
+  * @str: Fonction's parameter
+  * Return: void
+  */
+void puts_half(char *str)
+{
+	int n = 0;
+	int k;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	if (n % 2 == 1)
+	{
+		k = (n - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = n / 2;
+	}
+
+	for (; k < n; k++)
+	{
+		_putchar(str[k]);
+	}
+
+	_putchar('\n');
+}
