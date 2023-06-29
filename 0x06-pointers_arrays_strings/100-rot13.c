aee5c54ad069b6f914f91260bf4ae0a4b57aa019
Ouhassoun <ouhassounyoussef@gmail.com>
2023-06-29 06:48:35 -0700

    meow

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ * Return: String conversion
+ */
+
+char *rot13(char *s)
+{
+	int j = 0, x;
+	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[j] != '\0')
+	{
+		i = 0;
+		while (letters[i] != '\0')
+		{
+			if (s[j] == letters[i])
+			{
+				s[j] = rot13[i];
+				break;
+			}
+			i++;
+		}
+		j++;
+	}
+	return (s);
+}
