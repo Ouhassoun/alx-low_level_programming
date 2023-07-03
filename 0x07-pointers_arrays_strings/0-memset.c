210cc2297c8e142dd7d5aa606ab95805d5a171e2
Ouhassoun <ouhassounyoussef@gmail.com>
2023-07-03 05:47:55 -0700

    meow

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memset - fills memory with a constant byte
+ *@s: char
+ *@b: char
+ *@n: int
+ *
+ *Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while(n>0)
+	{
+		s[i] = b;
+		n--;
+		i++;
+	}
+	return (s);
+}
