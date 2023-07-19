fd7aa4fbff796ec14a26f6fd110ddeee6e61e7c3
Ouhassoun <ouhassounyoussef@gmail.com>
2023-07-19 15:17:14 -0700

    meow

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,18 +14,13 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+		{NULL, NULL},
+};
 
-	i = 0;
-	while (i < 5)
-	{
-		if (s[0] == ops[i].op[0])
-		{
-			return (ops[i].f);
-		}
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+
+	return (ops[i].f);
 }
