5080d5dfeef48dc66a8e5b128e403ab5821be71a
Ouhassoun <ouhassounyoussef@gmail.com>
2023-07-20 14:40:21 -0700

    meow

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,18 +7,22 @@
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
+    va_list args;
+    unsigned int i;
 
-	va_start(valist, n);
+    va_start(args, n);
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
-		printf("%s", separator);
-	}
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", va_arg(args, int));
 
-	printf("\n");
-	va_end(valist);
+        if (i != n - 1 && separator != NULL)
+        {
+            printf("%s", separator);
+        }
+    }
+
+    printf("\n");
+
+    va_end(args);
 }
