b6070ea4355ee3b8af3f370ba1baa59743372da5
Ouhassoun <ouhassounyoussef@gmail.com>
2023-08-07 18:41:40 -0700

    meow

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,47 +1,31 @@
 #include "main.h"
 
 /**
-  * _strlen - length of a string
-  * @s: char
-  * Return: int
-**/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-/**
-* append_text_to_file - appends text at the end of a file
-* @filename: char
-* @text_content: char
-* Return: int
-*/
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ * Return: int
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t nletters;
-	int file;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
+
+	if (text_content != NULL)
 	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	}
-	if (text_content)
-	{
-		nletters = write(file, text_content, _strlen(text_content));
-		if (nletters == -1)
-		{
-			close(file);
-			return (-1);
-		}
-	}
-	close(file);
+
+	close(o);
+
 	return (1);
 }
