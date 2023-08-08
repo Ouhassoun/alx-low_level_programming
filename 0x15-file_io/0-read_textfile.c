77c39af9fe1d4de196f0b5e851f9582b5afb3e01
Ouhassoun <ouhassounyoussef@gmail.com>
2023-08-07 18:35:14 -0700

    meow

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,27 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* read_textfile - check the code for Holberton School students.
-* @filename: char
-* @letters: size_t
-* Return: letters printed
-*/
-
+ * read_textfile - Reads a text file and prints it to the POSIX
+ * @filename: char
+ * @letters: size_t
+ * Return: ssize_t
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nletters;
-	int file;
-	char *text;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (!filename)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	text = malloc(sizeof(char) * letters + 1);
-	if (text == NULL)
-		return (0);
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-	{
-		free(text);
-		return (0);
-	}
-	nletters = read(file, text, sizeof(char) * letters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(file);
-		return (0);
-	}
-	nletters = write(STDOUT_FILENO, text, nletters);
-	if (nletters == -1)
-	{
-		free(text);
-		close(file);
-		return (0);
-	}
-	free(text);
-	close(file);
-	return (nletters);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
