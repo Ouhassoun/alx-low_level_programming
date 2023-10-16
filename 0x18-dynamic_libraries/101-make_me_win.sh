d03bd1bf3a24984cf28c4606bf6bf9bcb423facc
Jaguar <gitibillyzombie@gmail.com>
2023-10-16 12:48:59 -0700

    meow

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Ouhassoun/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp https://github.com/Ouhassoun/alx-low_level_programming/blob/master/0x18-dynamic_libraries/meow.so
+export LD_PRELOAD=/tmp/meow.so
